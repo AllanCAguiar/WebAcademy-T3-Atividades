bb364753d8ee2f846d8804d08619dcb6ea3d81ee
AllanCAguiar <allan.aguiar@icomp.ufam.edu.br>
2024-02-21 22:39:07 -0400

    Added lab2 and exerc1

---
+++ 03-Javascript/Exerc1/main.js
@@ -0,0 +1,33 @@
+let perdeu = false
+let escolhaJogador
+let escolhaCPU
+let vitorias=0
+while(perdeu==false){
+    console.log("Escolha sua jogada: \n1 - Papel \n2 - Pedra \n3 - Tesoura")
+    escolhaJogador = parseInt(prompt())
+    if(escolhaJogador!=1 && escolhaJogador!=2 && escolhaJogador!=3){
+        console.log("Jogada invalida, voce perdeu! A sua pontuacao foi de "+vitorias)
+        break
+    }
+    escolhaCPU = Math.floor(Math.random()*3)+1
+    if(escolhaCPU==1){
+        console.log("O computador jogou Papel")
+    }
+    else if(escolhaCPU==2){
+        console.log("O computador jogou Pedra")
+    }
+    else{
+        console.log("O computador jogou Tesoura")
+    }
+    if(escolhaCPU==escolhaJogador){
+        console.log("A rodada empatou!")
+    }
+    else if((escolhaJogador==1 && escolhaCPU==3) || (escolhaJogador==2 && escolhaCPU==1) || (escolhaJogador==3 && escolhaCPU==2)){
+        console.log("Voce perdeu! A sua pontuacao foi de "+vitorias)
+        perdeu=true
+    }
+    else{
+        console.log("Voce ganhou!")
+        vitorias++
+    }
+}
