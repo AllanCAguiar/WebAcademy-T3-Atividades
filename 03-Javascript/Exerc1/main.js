eeca5eedf948293c3cba253bc298fad652d6304b
AllanCAguiar <allan.aguiar@icomp.ufam.edu.br>
2024-03-08 20:56:07 -0400

    Added alerts to Javascript Exerc1

--- 03-Javascript/Exerc1/main.js
+++ 03-Javascript/Exerc1/main.js
@@ -3,10 +3,10 @@ let escolhaJogador
 let escolhaCPU
 let vitorias=0
 while(perdeu==false){
-    console.log("Escolha sua jogada: \n1 - Papel \n2 - Pedra \n3 - Tesoura")
-    escolhaJogador = parseInt(prompt())
+    console.log("Escolha sua jogada: \n1 - Papel \n2 - Pedra \n3 - Tesoura");
+    escolhaJogador = parseInt(prompt("Escolha sua jogada: \n1 - Papel \n2 - Pedra \n3 - Tesoura"));
     if(escolhaJogador!=1 && escolhaJogador!=2 && escolhaJogador!=3){
-        console.log("Jogada invalida, voce perdeu! A sua pontuacao foi de "+vitorias)
+        console.log("Jogada invalida, voce perdeu! A sua pontuacao foi de "+vitorias);
         break
     }
     escolhaCPU = Math.floor(Math.random()*3)+1
@@ -25,6 +25,7 @@ while(perdeu==false){
     else if((escolhaJogador==1 && escolhaCPU==3) || (escolhaJogador==2 && escolhaCPU==1) || (escolhaJogador==3 && escolhaCPU==2)){
         console.log("Voce perdeu! A sua pontuacao foi de "+vitorias)
         perdeu=true
+        window.alert("Voce perdeu! A sua pontuacao foi de "+vitorias);
     }
     else{
         console.log("Voce ganhou!")
