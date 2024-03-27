1eca2e8c99c83233d4a215924485520da9fec8e0
AllanCAguiar <allan.aguiar@icomp.ufam.edu.br>
2024-03-27 18:48:11 -0400

    Express Exercise 1

---
+++ 09-Express/Express/src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT||8000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.listen(PORT, () => {
+    console.log(`Express app iniciada na porta ${PORT}.`);
+});
