50dd89d7ce41fcf0558d4ae44dd828ad253eec1c
AllanCAguiar <allan.aguiar@icomp.ufam.edu.br>
2024-05-07 20:59:50 -0400

    Initialize React TP2

---
+++ 14-REACT/ultimate-lojinha/src/app/compononents/ListagemProdutos.tsx
@@ -0,0 +1,25 @@
+import CardProduto from "./CardProduto";
+
+interface ListagemProdutosProps {
+  produtos: Produto[] | null;
+  adicionarAoCarrinho: (produto: Produto) => void;
+}
+
+export default function ListagemProdutos({ produtos, adicionarAoCarrinho }: ListagemProdutosProps) {
+  return (
+    <>
+      <h5 className="mb-3">Produtos disponíveis:</h5>
+      {produtos ? (
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-3">
+          {produtos.map((produto) => (
+            <CardProduto
+              key={produto.id}
+              produto={produto}
+              adicionarAoCarrinho={adicionarAoCarrinho}
+            />
+          ))}
+        </div>
+      ) : null}
+    </>
+  );
+}
---
+++ 14-REACT/ultimate-lojinha/src/app/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import ResumoCarrinho from "./compononents/ResumoCarrinho";
+import ListagemProdutos from "./compononents/ListagemProdutos";
+
+export default function Produtos() {
+  const [produtos, setProdutos] = useState<Produto[] | null>(null);
+
+  useEffect(() => {
+    const fetchProdutos = async () => {
+      try {
+        const response = await fetch("https://ranekapi.origamid.dev/json/api/produto/");
+        const json = await response.json();
+        setProdutos(json);
+      } catch (err) {
+        console.error("Erro ao buscar dados ", err);
+      }
+    };
+    fetchProdutos();
+  }, []);
+
+  const [precoTotal, setPrecoTotal] = useState<number>(0);
+  const [quantItens, setQuantItens] = useState<number>(0);
+
+  const adicionarAoCarrinho = (produto: Produto) => {
+    setQuantItens(quantItens + 1);
+    setPrecoTotal(precoTotal + parseFloat(produto.preco));
+  };
+
+  return (
+    <>
+      <main>
+        <div className="container p-5">
+          <ResumoCarrinho quantItens={quantItens} precoTotal={precoTotal} />
+          <ListagemProdutos produtos={produtos} adicionarAoCarrinho={adicionarAoCarrinho} />
+        </div>
+      </main>
+    </>
+  );
+}
