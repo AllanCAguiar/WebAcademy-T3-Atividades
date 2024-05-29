e5382aa3ec26553370aa6e1c2e508d19c7e690bc
AllanCAguiar <allan.aguiar@icomp.ufam.edu.br>
2024-05-28 20:26:36 -0400

    Finished

---
+++ 16-FundamentosTeste/Trab1/src/tests/validation.test.js
@@ -0,0 +1,66 @@
+const {
+    firstName,
+    verifyStockAvailability,
+    calculateTotalPrice,
+} = require("../utils/validations")
+
+describe("firstName()", () => {
+    it("Deve retornar apenas o primeiro nome", () => {
+        const fullName = "Arthur Morgan";
+        const result = firstName(fullName);
+        expect(result).toBe("Arthur")
+    });
+
+    it("Deve retornar o mesmo nome quando não encontra espaços em branco", () => {
+        const name = "Arthur";
+        const result = firstName(name);
+        expect(result).toBe(name)
+    });
+
+    it("Deve retornar o primeiro nome corretamente quando encontra espaço em branco no começo", () => {
+        const fullName = " Arthur Morgan";
+        const result = firstName(fullName);
+        expect(result).toBe("Arthur")
+    });
+
+    it("Deve retornar o primeiro nome corretamente quando encontra espaço em branco no final", () => {
+        const fullName = "Arthur Morgan ";
+        const result = firstName(fullName);
+        expect(result).toBe("Arthur")
+    });
+});
+
+describe("veriverifyStockAvailability()", () => {
+    it("Deve retornar falso para um produto que não existe", () =>{
+        const name = "bicycle"
+        const result = verifyStockAvailability(name,)
+        expect(result).toBeFalsy()
+    });
+    it("Deve retornar falso para um produto fora de estoque", () =>{
+        const name = "book"
+        const result = verifyStockAvailability(name, )
+        expect(result).toBeTruthy()
+    });
+    it("Deve retornar verdadeiro para um produto que está no estoque", () =>{
+        const name = "bicycle"
+        const result = verifyStockAvailability(name, )
+        expect(result).toBeTruthy()
+    });
+});
+
+describe("calculateTotalPrice()", () => {
+    it("Deve retornar a soma dos preços dos produtos", () =>{
+        const products = [
+                 { name: 'Product 1', price: 0, quantity: 2 },
+                 { name: 'Product 2', price: 0, quantity: 2 },
+                 { name: 'Product 3', price: 0, quantity: 1 }
+                ]
+        let soma = 0
+        for(let i = 0; i<products.length; i++){
+            soma+=products[i].price
+        }
+        const result = calculateTotalPrice(products)
+        expect(result).toBe(soma)
+
+    });
+});
